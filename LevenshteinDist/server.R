47bb21de80ac4b1451181e13e956770631cb6a3b
Song Li <songl@outlook.com>
2020-03-31 22:16:29 +0800

    Add cosine similarity

--- LevenshteinDist/server.R
+++ LevenshteinDist/server.R
@@ -2,12 +2,21 @@ shinyServer(
   function(input, output) {
     library(textTinyR)
     library(stringr)
-    score <- reactive({
-      str1 <- trimws(input$a)
-      str2 <- trimws(input$b)
-      1 - levenshtein_distance(str1, str2) / max(str_length(c(str1, str2)))
-    })
+
+    str1 <- reactive({trimws(input$a)})
     
-    output$result <- renderText(score())
+    str2 <- reactive({trimws(input$b)})
+    
+    output$levenshtein <- renderText({
+      req(str1())
+      req(str2())
+      1 - levenshtein_distance(str1(), str2()) / max(str_length(c(str1(), str2())))
+    })
+
+    output$cosine <- renderText({
+      req(str1())
+      req(str2())
+      cosine_distance(str1(), str2())
+    })
   }
 )
--- LevenshteinDist/ui.R
+++ LevenshteinDist/ui.R
@@ -5,18 +5,22 @@ shinyUI(fluidPage(
     column(3,
            textInput("a",
                         label = h3("A"),
-                        value = "Hello world!")),
+                        value = "Hello world")),
     
     column(3,
            textInput("b",
                         label = h3("B"),
-                        value = "Hello world again..."))
+                        value = "Hello world again"))
     ),
 
   mainPanel(
-    textOutput("result"),
+    helpText("Similarity between 1 (the same) and 0 (totally different)"),
     tags$hr(),
-    helpText("Similarity between 1 (the same) and 0 (totally different) = 1 - ", a("Levenshtein distance", href="https://en.wikipedia.org/wiki/Levenshtein_distance")),
+    textOutput("levenshtein"),
+    helpText("1 - ", a("Levenshtein distance", href="https://en.wikipedia.org/wiki/Levenshtein_distance"), " / characters of the longer string"),
+    textOutput("cosine"),
+    helpText(a("Cosine similarity", href="https://en.wikipedia.org/wiki/Cosine_similarity"), "calculated by word"),
+    tags$hr(),
     helpText("View", a("source code on GitHub", href="https://github.com/boltomli/MyShinyApps"))
   )
 ))
