4ff901c4a8f060cce7e0cd0ae56a954bd4b16dfa
Song Li <songl@outlook.com>
2020-06-10 08:57:23 +0800

    select by all types

--- Homophones/server.R
+++ Homophones/server.R
@@ -4,30 +4,26 @@ shinyServer(
     library(Unicode)
 
     Unihan_Readings <- read.delim("raw/Unihan_Readings.txt", header=FALSE, comment.char="#", stringsAsFactors=FALSE, col.names = c("code", "key", "value"))
+    types <- read.table("unihan_readings/types.csv", col.names = c("type"))
 
-    lang_input <- reactive({
-      switch (input$lang,
-        "汉语（普通话 / Mandarin）" = "kMandarin",
-        "汉语（粤语 / Cantonese）" = "kCantonese",
-        "韩语（Korean / Hangul）" = "kKorean",
-        "日语（Japanese / On）" = "kJapaneseOn",
-      )
+    output$lang_selector <- renderUI({
+      selectInput("lang", "选择一个语言或方言：",
+                  choices = types$type)
     })
 
     pronunciations <- reactive({
-      req(lang_input())
       zi <- trimws(input$zi)[[1]]
       zi <- substr(zi, start = nchar(zi), stop = nchar(zi))
       cp <- paste("U+", toupper(sprintf("%x", as.numeric(stri_enc_toutf32(zi)))), sep = "")
       split_by <- " "
-      if (lang_input() == "kHanyuPinyin") split_by <- "[ :,0-9\\.]"
-      strsplit(as.character(Unihan_Readings$value[which(lang_input() == Unihan_Readings$key & Unihan_Readings$code == cp)]), split_by)[[1]]
+      if (input$lang %in% c("kHanyuPinyin", "kTGHZ2013", "kXHC1983")) split_by <- "[ :,0-9\\.]"
+      strsplit(as.character(Unihan_Readings$value[which(input$lang == Unihan_Readings$key & Unihan_Readings$code == cp)]), split_by)[[1]]
     })
 
     characters <- reactive({
       req(pronunciations())
       zi_same_p <- unlist(lapply(pronunciations(), function(x) {
-        Unihan_Readings$code[which(lang_input() == Unihan_Readings$key & sapply(strsplit(as.character(Unihan_Readings$value), "[ :,]"), function(x, y) y %in% x, x))]}))
+        Unihan_Readings$code[which(input$lang == Unihan_Readings$key & sapply(strsplit(as.character(Unihan_Readings$value), "[ :,]"), function(x, y) y %in% x, x))]}))
       unlist(lapply(as.list(zi_same_p), function(x) {
         u_char_inspect(as.character(x))['Char']
       }))
--- Homophones/ui.R
+++ Homophones/ui.R
@@ -5,8 +5,7 @@ shinyUI(fluidPage(
        textInput("zi",
                  label = h3("输入单个汉字"),
                  value = "好"),
-       selectInput("lang", "选择一个语言或方言：",
-                   choices = c("韩语（Korean / Hangul）", "汉语（普通话 / Mandarin）", "汉语（粤语 / Cantonese）", "日语（Japanese / On）")),
+       uiOutput("lang_selector"),
      ),
     mainPanel(
       htmlOutput("pronunciation"),
