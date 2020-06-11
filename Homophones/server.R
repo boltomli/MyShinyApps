63009ef6cb52824d7408446a2a182eda74ae87aa
Song Li <songl@outlook.com>
2020-06-11 16:33:29 +0800

    refactor to use database

--- Homophones/server.R
+++ Homophones/server.R
@@ -1,42 +1,53 @@
 shinyServer(
   function(input, output) {
     library(stringi)
-    library(Unicode)
+    library(DBI)
+    library(RSQLite)
+    library(rlang)
 
-    Unihan_Readings <- read.delim("raw/Unihan_Readings.txt", header=FALSE, comment.char="#", stringsAsFactors=FALSE, col.names = c("code", "key", "value"))
-    types <- read.table("unihan_readings/types.csv", col.names = c("type"))
+    con <- dbConnect(SQLite(), "unihan_readings.db")
+    chars <- dbReadTable(con, "chars")
+    types <- dbReadTable(con, "types")
+
+    dicts <- reactive({
+      d <- list()
+      lapply(types$Type, function(x) {
+        d[[x]] <<- dbReadTable(con, x)
+      })
+      d
+    })
 
     output$lang_selector <- renderUI({
       selectInput("lang", "选择一个语言或方言：",
-                  choices = types$type)
+                  choices = types$Type)
     })
 
     pronunciations <- reactive({
+      req(dicts())
       zi <- trimws(input$zi)[[1]]
       zi <- substr(zi, start = nchar(zi), stop = nchar(zi))
-      cp <- paste("U+", toupper(sprintf("%x", as.numeric(stri_enc_toutf32(zi)))), sep = "")
-      split_by <- " "
-      if (input$lang %in% c("kHanyuPinyin", "kTGHZ2013", "kXHC1983")) split_by <- "[ :,0-9\\.]"
-      strsplit(as.character(Unihan_Readings$value[which(input$lang == Unihan_Readings$key & Unihan_Readings$code == cp)]), split_by)[[1]]
+      cp <- chars[chars$Char == zi,]$Code
+      if (!is_empty(cp)) {
+        dict <- dicts()[[input$lang]]
+        dict[dict$Code == cp,]$Value
+      } else {
+        paste0(zi, " is not a known character.")
+      }
     })
 
-    characters <- reactive({
-      req(pronunciations())
-      zi_same_p <- unlist(lapply(pronunciations(), function(x) {
-        Unihan_Readings$code[which(input$lang == Unihan_Readings$key & sapply(strsplit(as.character(Unihan_Readings$value), "[ :,]"), function(x, y) y %in% x, x))]}))
-      unlist(lapply(as.list(zi_same_p), function(x) {
-        u_char_inspect(as.character(x))['Char']
-      }))
-    })
-    
     output$pronunciation <- renderText({
       req(pronunciations())
       pronunciations()
     })
 
     output$result <- renderText({
-      req(characters())
-      characters()
+      req(pronunciations())
+      dict <- dicts()[[input$lang]]
+      zi_same_p <- unlist(lapply(pronunciations(), function(x) {
+        dict[dict$Value == x,]$Code}))
+      unlist(lapply(as.list(zi_same_p), function(x) {
+        chars[chars$Code == x,]$Char
+      }))
     })
   }
 )
